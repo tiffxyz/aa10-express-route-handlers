fb041b237af892e0923019143f06bcc0bfa9a0e2
tiffxyz <tiffanie.lyon@gmail.com>
2024-08-04 18:13:56 -0500

    mainand bonus done

--- server/app.js
+++ server/app.js
@@ -24,7 +24,31 @@ const {
 const express = require('express');
 const app = express();
 
-// Your code here 
+// Your code here
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+app.get('/artists', (req, res) => {
+  res.json(getAllArtists())
+});
+
+app.post('/artists', (req, res) => {
+  res.status(201)
+  let artist = addArtist(req.body)
+  res.json(artist)
+});
+
+app.get('/artists/latest', (req, res) => {
+  res.json(getLatestArtist())
+});
+
+app.get('/artists/latest/albums', (req, res) => {
+  res.json(getAlbumsForLatestArtist())
+});
 
 // DO NOT MODIFY
 if (require.main === module) {
